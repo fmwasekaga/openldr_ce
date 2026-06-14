a1ce020b8282730a621aa327ae9ddcb4c0b2f72f
Fredrick Lusako Mwasekaga <fmwasekaga@users.noreply.github.com>
2026-06-14 17:40:36 +0300

    feat(web): Tailwind v4 + shadcn token bridge, cn() util, Radix test polyfills (P2-DOCUI-1)

--- apps/web/src/setupTests.ts
+++ apps/web/src/setupTests.ts
@@ -1,1 +1,9 @@
 import '@testing-library/jest-dom/vitest';
+
+// Radix UI (Select/Dialog/Dropdown) uses pointer-capture + scrollIntoView, which jsdom lacks.
+if (typeof Element !== 'undefined') {
+  if (!Element.prototype.hasPointerCapture) Element.prototype.hasPointerCapture = () => false;
+  if (!Element.prototype.setPointerCapture) Element.prototype.setPointerCapture = () => {};
+  if (!Element.prototype.releasePointerCapture) Element.prototype.releasePointerCapture = () => {};
+  if (!Element.prototype.scrollIntoView) Element.prototype.scrollIntoView = () => {};
+}
