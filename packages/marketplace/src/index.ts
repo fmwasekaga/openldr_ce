6e5fcf66289390f402032d2f54f681cf473f145a
Fredrick Lusako Mwasekaga <fmwasekaga@users.noreply.github.com>
2026-06-22 13:17:10 +0300

    feat(marketplace): barrel exports

--- packages/marketplace/src/index.ts
+++ packages/marketplace/src/index.ts
@@ -1,1 +1,7 @@
-export {};
+export * from './capabilities';
+export * from './artifact-manifest';
+export * from './bundle';
+export * from './signing';
+export * from './trust';
+export * from './trust-store';
+export * from './compatibility';
