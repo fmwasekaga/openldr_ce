bf229a2d555d2b2043275e671901576639b0d130
Fredrick Lusako Mwasekaga <fmwasekaga@users.noreply.github.com>
2026-06-23 08:07:38 +0300

    feat(workflows): public package exports

--- packages/workflows/src/index.ts
+++ packages/workflows/src/index.ts
@@ -1,1 +1,3 @@
-export {};
+export * from './types';
+export { createWorkflowStore, type WorkflowStore } from './store';
+export { runWorkflow, topologicalSort, type WorkflowRunResult, type NodeRunResult, type RunWorkflowOptions } from './engine/run-workflow';
